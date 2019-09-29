29180c78686c46b94a1482598ea99fcee7d24012
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-09-29 14:53:10 +0200

    second

---
+++ labirinthe.py
@@ -0,0 +1,12 @@
+class Lab(object):
+	def __init__(self,reglage,size=15):
+		self.format={}
+		self.reglage=reglage
+		self.size=size
+	def initialisermap(self):
+		positions=[]
+		for i in range(size):
+			for j in range(size):
+				positions.append((i,j))
+
+		self.reglage=
