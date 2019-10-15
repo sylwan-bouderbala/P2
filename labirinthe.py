be49b643506814892fe297ce4248fc19ef39606c
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-10-15 15:07:52 +0200

    1.0

--- labirinthe.py
+++ labirinthe.py
@@ -1,26 +1,37 @@
+import os 
+
+
 class Lab(object):
 	def __init__(self,reglage,size=15):
 		self.format={}
-		self.reglage=reglage
+		self.reglage=reglage.read()
 		self.size=size
-	def initialisermap(self):
+	def initialisermap(self,size=15):
 		positions=[]
-		for i in range(size):
-			for j in range(size):
+		for i in range(1,15):
+			for j in range(1,15):
 				positions.append((i,j))
 
-		file=self.reglage.read()
+		self.reglage
 		liste2=[]
-		for caracters in file:
+		for caracters in self.reglage:
 			if caracters != "\n":
-			    liste2.append()
+			    liste2.append(caracters)
 			else :
 				pass 
-		for x in range(size*size):
+
+		for x in range(size**2):
 			self.format[positions[x]]=liste2[x]
-	def affichermap(self):
-		
+	def affichermap(self,size=15):
+		for i in range(size):
+			listeaffichage=""
+			print(listeaffichage)
+			for j in range(size):
+				listeaffichage= self.format[(i,j)] + listeaffichage
+			print(listeaffichage)
 
-			
 
-
+reglage=open("reglage.txt","r")			
+Lab=Lab(reglage)
+Lab.initialisermap()
+Lab.affichermap()
