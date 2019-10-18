955472ab514f651ca96c991db1b704b15ba3d05f
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-10-18 15:40:27 +0200

    rr

--- labirinthe.py
+++ labirinthe.py
@@ -8,26 +8,29 @@ class Lab(object):
 		self.size=size
 	def initialisermap(self,size=15):
 		positions=[]
-		for i in range(1,15):
-			for j in range(1,15):
+		for i in range(15):
+			for j in range(15):
 				positions.append((i,j))
 
 		self.reglage
 		liste2=[]
 		for caracters in self.reglage:
 			if caracters != "\n":
-			    liste2.append(caracters)
+			    liste2.append(caracters+" ")
 			else :
 				pass 
-
-		for x in range(size**2):
+		print(liste2)
+             
+		for x in range(size*size):
 			self.format[positions[x]]=liste2[x]
+		print(self.format)
+
 	def affichermap(self,size=15):
 		for i in range(size):
 			listeaffichage=""
 			print(listeaffichage)
 			for j in range(size):
-				listeaffichage= self.format[(i,j)] + listeaffichage
+				listeaffichage= listeaffichage + self.format[(i,j)] 
 			print(listeaffichage)
 
 
