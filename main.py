7321fbdb105b3c9df849305ab15e85cf1777d203
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2019-11-23 21:26:50 +0100

    commit to add perso id

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import labirinthe as Lab
-import perso as perso
 
 tab= Lab.Lab()
 tab.init()
--- perso.py
+++ perso.py
@@ -2,6 +2,15 @@ import labirinthe
 import os
 
 class Perso(object):
-	def __init__(self, arg):
-		
+	def __init__(self):
+		self.position=()
+
+	def id(self,tab):
+		if 1 in tab.values():
+			self.id==1
+		if 2 in tab.values():
+			self.id==2
+			
+	def position(self,tab):
+		for 
 
