3ff2f66f0fb8af4e8d002fbe98c75cfda3ed9023
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2020-05-29 15:51:42 +0200

    Dernier Pygame 23/04/20

--- graph.py
+++ graph.py
@@ -1,19 +1,24 @@
 import pygame
 # par la même occasion cela importe pygame.locals dans l'espace de nom de Pygame
 
-pygame.init()
 
-Screen = pygame.display.set_mode((300, 300))
+class dysplay():
+    def __init__(self):
+        pygame.init()
+        self.ecran = pygame.display.set_mode((600, 600))
+        McGiversrc = 'images/MacGyver.png'
+        Mechantsrc='images/Gardien.png'
+        Wallsrc='images/structures.png'
+        seringuesrc='images/seringue.png'
+        tubesrc="images/tube_plastique.png"
+        ethersrc="images/ether.png"
 
-Continue = True
 
-while Continue:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            Continue = False
-pygame.quit()
+        self.McGiver=pygame.image.load(McGiversrc).convert()
+        self.Mechant=pygame.image.load(Mechantsrc).convert()
+        self.Wall=pygame.image.load(Wallsrc).convert()
+        self.seringue=pygame.image.load(seringuesrc).convert()
+        self.tube=pygame.image.load(tubesrc).convert()
+        self.ether=pygame.image.load(ethersrc).convert()
 
-McGiver = 'MacGyver.png'
-Mechant='Gardien.png'
-Wall='structures.png'
-seringue='seringue.png'
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from labirinthe import Lab
+import pygame
+from graph import dysplay
 
 class game(object):
     def __init__(self):
@@ -18,5 +20,30 @@ class game(object):
                 self.display.Display()
         print('c\'est gagner')
 
+pygame.init()
+ecran = pygame.display.set_mode((600, 600))
+McGiversrc = 'images/MacGyver.png'
+Mechantsrc = 'images/Gardien.png'
+Wallsrc = 'images/structures.png'
+seringuesrc = 'images/seringue.png'
+tubesrc = "images/tube_plastique.png"
+ethersrc = "images/ether.png"
 
-Game = game()
+McGiver = pygame.image.load(McGiversrc).convert()
+Mechant = pygame.image.load(Mechantsrc).convert()
+Wall = pygame.image.load(Wallsrc).convert()
+seringue = pygame.image.load(seringuesrc).convert()
+tube = pygame.image.load(tubesrc).convert()
+ether = pygame.image.load(ethersrc).convert()
+ecran = pygame.display.set_mode((600, 600))
+
+continuer = True
+for i in range(15):
+    for j in range(15):
+        ecran.blit(McGiver, (i,j))
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            continuer = False
+    pygame.display.flip()
+pygame.quit()
