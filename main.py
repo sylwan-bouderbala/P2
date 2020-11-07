04f363f838cd708679a24af596e1e17278358f94
sylwan bouderbala <sylwanbouderbala@outlook.fr>
2020-11-07 16:17:35 +0100

    Dernier commit sans Pygame 23/04/20

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from labirinthe import Lab
-import pygame
+# import pygame
+from perso import Perso
 from graph import dysplay
+
+class game(object):
+    def __init__ (self):
+        self.Map = Lab()
+        self.player = Perso()
+    def wininggame (self):
+        return false
+    def instance (self):
+        pygame.init()
+        gameDisplay = pygame.display.set_mode((self.Map.size*20,self.Map.size*20 ))
+
+
+game = game()
+
+print(game.Map)
--- perso.py
+++ perso.py
@@ -1,12 +1,12 @@
-import os
 
 class Perso(object):
     def __init__(self,positon):
         self.position = positon
-        self.objectnumber=0
+        self.objectnumber= 0
 
     def Addobject(self):
         self.objectnumber+=1
+
 perso = Perso([0])
 print(perso.objectnumber)
 perso.Addobject()
